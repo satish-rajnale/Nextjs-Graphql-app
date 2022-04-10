22c1bd3418b39b0eab82403649f53f6af9cbe1ed
Satish Rajnale <76589507+satish-rajnale@users.noreply.github.com>
2022-04-10 20:11:14 +0530

    condig webpack

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  webpack: (config, { isServer }) => {
+    if (!config.experiments) {
+      config.experiments = {};
+    }
+    config.experiments.topLevelAwait = true;
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,6 +1,37 @@
 import { ApolloServer } from "apollo-server-micro";
 import "reflect-metadata";
-const server = new ApolloServer({});
+import {
+  buildSchema,
+  ObjectType,
+  Resolver,
+  Query,
+  Arg,
+  Field,
+  ID,
+} from "type-graphql";
+
+@ObjectType()
+export class Dog {
+  @Field(() => ID)
+  name: string;
+
+  // @Field()
+  // type: string;
+}
+
+@Resolver(Dog)
+export class DogResolver {
+  @Query(() => [Dog])
+  dogs(): Dog[] {
+    return [{ name: "Bo" }, { name: "Bobby" }];
+  }
+}
+
+const schema = await buildSchema({
+  resolvers: [DogResolver],
+});
+
+const server = new ApolloServer({ schema });
 
 export const config = {
   api: { bodyParser: false },
